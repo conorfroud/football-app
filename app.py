b368ab1e9cd436a2f37d30d65eb75f7d53f77bdf
conorfroud <47294193+conorfroud@users.noreply.github.com>
2023-10-06 09:49:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def main_tab(df2):
                     (df2['player_market_value_euro'] <= player_market_value_range[1])]
 
     # Specify the columns you want to display in the final table
-    selected_columns = ['Player Name', 'Age', 'Team', 'League', 'Position', 'Score Type', 'Stoke Score', 'contract_expiry', 'player_market_value_euro']
+    selected_columns = ['Player Name', 'Age', 'Team', 'League', 'Position', 'Score Type', 'Stoke Score', 'Contract expires', 'Market value (millions)']
 
     # Display the filtered DataFrame with selected columns
     st.table(filtered_df[selected_columns])
