312b4ac267d8d1751a9ffcd967c9153c2e88720b
conorfroud <47294193+conorfroud@users.noreply.github.com>
2023-10-11 15:03:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -349,7 +349,7 @@ df = pd.read_csv("belgiumdata.csv")
 df2 = pd.read_csv("championshipscores.csv")
 
 # Create the navigation menu in the sidebar
-selected_tab = st.sidebar.radio("Navigation", ["Stoke Score", "Player Profile", "Scatter Plot"])
+selected_tab = st.sidebar.radio("Navigation", ["Stoke Score", "Scatter Plot"])
 
 # Based on the selected tab, display the corresponding content
 if selected_tab == "Stoke Score":
