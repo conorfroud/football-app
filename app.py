f90ff72191d0cf20629cc6dac1ae0ec6aabb0348
conorfroud <47294193+conorfroud@users.noreply.github.com>
2023-10-26 11:52:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -168,10 +168,10 @@ def about_tab(df2):
         columns = ["Player Name", "Deep Progressions (6)", "OBV Pass (6)", "Dribbles (6)", "OBV Dribble & Carry (6)", "Tackle/Dribbled Past % (6)", "PAdj Tackles (6)", "PAdj Interceptions (6)",  "Average Distance (6)", "Top 5 PSV-99 (6)"]
         plot_title = f"Defensive Midfield Metric Percentiles for {selected_player}"
     elif selected_profile == "Left Back":
-        columns = ["Player Name", "PAdj Tackles (LB)", "PAdj Interceptions (LB)", "OBV Defensive Action (LB)", "OBV Dribble & Carry (LB)", "Tackle/Dribbled Past (LB)", "Open Play xA (LB)", "Successful Crosses (LB)",  "Average Distance (LB)", "Top 5 PSV-99 (LB)"]
+        columns = ["Player Name", "PAdj Tackles (LB)", "PAdj Interceptions (LB)", "Tackle/Dribbled Past (LB)", "OBV Defensive Action (LB)", "OBV Dribble & Carry (LB)",  "Open Play xA (LB)", "Successful Crosses (LB)",  "Average Distance (LB)", "Top 5 PSV-99 (LB)"]
         plot_title = f"Left Back Metric Percentiles for {selected_player}"
     elif selected_profile == "Right Back":
-        columns = ["Player Name", "PAdj Tackles (RB)", "PAdj Interceptions (RB)", "OBV Defensive Action (RB)", "OBV Dribble & Carry (RB)", "Tackle/Dribbled Past (RB)", "Open Play xA (RB)", "Successful Crosses (RB)",  "Average Distance (RB)", "Top 5 PSV-99 (RB)"]
+        columns = ["Player Name", "PAdj Tackles (RB)", "PAdj Interceptions (RB)", "Tackle/Dribbled Past (RB)", "OBV Defensive Action (RB)", "OBV Dribble & Carry (RB)", , "Open Play xA (RB)", "Successful Crosses (RB)",  "Average Distance (RB)", "Top 5 PSV-99 (RB)"]
         plot_title = f"Right Back Metric Percentiles for {selected_player}"
     elif selected_profile == "Centre Back":
         columns = ["Player Name", "Aerial Wins (CB)", "Aerial Win % (CB)", "PAdj Interceptions (CB)", "PAdj Tackles (CB)", "OBV Pass (CB)", "OBV Dribble & Carry (CB)", "OBV Defensive Action",  "Average Distance (CB)", "Top 5 PSV-99 (CB)"]
