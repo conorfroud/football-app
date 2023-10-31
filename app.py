837253464083a7c38ed4ca398d597ed310d47d24
conorfroud <47294193+conorfroud@users.noreply.github.com>
2023-10-31 14:52:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -310,29 +310,32 @@ def scatter_plot(df):
 def comparison_tab(df):
 
 # Title and description
-    st.title("Player Comparison App")
-    st.write("Select players and metrics to compare in a table.")
+st.title("Player Comparison App")
+st.write("Select players and metrics to compare in a table.")
 
 # Sidebar: Player selection
-    selected_players = st.sidebar.multiselect("Select Players", df["player_name"])
+selected_players = st.sidebar.multiselect("Select Players", df["player_name"])
 
 # Sidebar: Metric selection
-    selected_metrics = st.sidebar.multiselect("Select Metrics", df.columns[1:])
+selected_metrics = st.sidebar.multiselect("Select Metrics", df.columns[1:])
 
 # Filter the DataFrame based on selected players
-    filtered_df = df[df["player_name"].isin(selected_players)]
+filtered_df = df[df["player_name"].isin(selected_players)]
 
-# Display the table
-    if selected_metrics:
-      st.write("Comparison Table")
-      if filtered_df.empty:
-         st.warning("No players selected. Please select at least one player.")
-      else:
-         selected_columns = ["player_name"] + selected_metrics
-         st.dataframe(filtered_df[selected_columns], width=700, height=400, hide_index=True)
+# Define a function for conditional formatting
+def highlight_best_player(s):
+    is_best = s == s.max()
+    return ['background-color: yellow' if v else '' for v in is_best]
+
+# Display the table with conditional formatting
+if selected_metrics:
+    st.write("Comparison Table")
+    if filtered_df.empty:
+        st.warning("No players selected. Please select at least one player.")
     else:
-       st.warning("Select at least one metric to compare.")
-
+        selected_columns = ["player_name"] + selected_metrics
+        st.dataframe(filtered_df[selected_columns].style.apply(highlight_best_player, subset=selected_metrics), hide_index=True)
+    st.warning("Select at least one metric to compare.")
 
 # Load the DataFrame
 df = pd.read_csv("belgiumdata.csv")
