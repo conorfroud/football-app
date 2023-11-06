94d91eb6d0c21c5e61125b7550218cb4728ce654
conorfroud <47294193+conorfroud@users.noreply.github.com>
2023-11-06 16:20:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -343,7 +343,7 @@ def comparison_tab(df):
         if filtered_df.empty:
             st.warning("No players selected. Please select at least one player.")
         else:
-            selected_columns = ["Player Name"] + ["Minutes"] + selected_metrics
+            selected_columns = ["Player Name"] + selected_metrics
             if total_option:
                 formatted_df = calculated_df[selected_columns].copy()
             else:
