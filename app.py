4451bf83b715746287ae4b0c66de4d0ecf122d75
conorfroud <47294193+conorfroud@users.noreply.github.com>
2024-01-08 15:42:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -115,10 +115,10 @@ def main_tab(df2):
         (df2['Market value (millions)'] <= player_market_value_range[1]) &
         (df2['Stoke Score'] >= stoke_range[0]) &
         (df2['Stoke Score'] <= stoke_range[1]) &
-        (df2[selected_columns[5]] >= avg_distance_percentile_range[0]) &
-        (df2[selected_columns[5]] <= avg_distance_percentile_range[1]) &
-        (df2[selected_columns[6]] >= top_5_psv_99_percentile_range[0]) &
-        (df2[selected_columns[6]] <= top_5_psv_99_percentile_range[1]) &
+        (df2[selected_columns[6]] >= avg_distance_percentile_range[0]) &
+        (df2[selected_columns[6]] <= avg_distance_percentile_range[1]) &
+        (df2[selected_columns[7]] >= top_5_psv_99_percentile_range[0]) &
+        (df2[selected_columns[7]] <= top_5_psv_99_percentile_range[1]) &
         (df2['L/R Footedness %'] >= lr_footedness_range[0]) &
         (df2['L/R Footedness %'] <= lr_footedness_range[1])
     ]
