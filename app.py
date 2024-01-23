4baec09889fed78b4c4a9922850902f4a21867ff
conorfroud <47294193+conorfroud@users.noreply.github.com>
2024-01-23 14:31:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -742,9 +742,9 @@ def player_stat_search(df):
     if selected_leagues:
         filtered_df = filtered_df[filtered_df['League'].isin(selected_leagues)]
 
-    # Display the customized table with 'Age' as a constant column
+    # Display the customized table with 'Age' as a constant column without index numbering
     selected_stats_ordered = always_included_columns + [col for col in selected_stats if col not in always_included_columns]
-    st.write(filtered_df[selected_stats_ordered])
+    st.write(filtered_df[selected_stats_ordered], use_container_width=True) 
 
 # Load the DataFrame
 df = pd.read_csv("belgiumdata.csv")
