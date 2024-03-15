11772ad4d8b65474bc2ed8e8f6272c1dd140e737
conorfroud <47294193+conorfroud@users.noreply.github.com>
2024-03-15 10:03:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from mplsoccer import Pitch
 from mplsoccer import PyPizza
 from PIL import Image
 from sklearn.metrics.pairwise import cosine_similarity
+from streamlit_gsheets import GSheetsConnection
 
 st.set_page_config(layout="wide")
 
