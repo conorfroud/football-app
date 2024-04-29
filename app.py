a77a3c87c7c489e028c09b65f8a09640eae8acae
conorfroud <47294193+conorfroud@users.noreply.github.com>
2024-04-29 13:53:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1046,16 +1046,8 @@ def plot_players_on_pitch(rb_players_data, lb_players_data, lw_players_data, rw_
 
     st.pyplot(fig)
 
-import streamlit as st
-import pandas as pd
-import requests
-import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
-
 def streamlit_interface():
     
-    # Pull data from Google Sheets
     url = "https://docs.google.com/spreadsheets/d/1GAghNSTYJTVVl4I9Q-qOv_PGikuj_TQIgSp2sGXz5XM/edit?usp=sharing"
     url1 = "https://docs.google.com/spreadsheets/d/1GAghNSTYJTVVl4I9Q-qOv_PGikuj_TQIgSp2sGXz5XM/edit#gid=1930222963"
     conn = st.connection("gsheets", type=GSheetsConnection)
@@ -1075,27 +1067,42 @@ def streamlit_interface():
     # Display player info card visualization
     st.markdown(f"### {selected_player} ###", unsafe_allow_html=True)
     
-    # Extract image URL from the cell
-    image_url = filtered_data['Image'].iloc[0]
-    if not pd.isnull(image_url):  # Check if image_url is not NaN
-        # Fetch image from Google Drive
-        response = requests.get(image_url)
-        if response.status_code == 200:
-            st.image(response.content, width=140)
-        else:
-            st.write("Image not available")
-    else:
-        st.write("No image available")
+    # Create three columns layout
+    col1, col2, col3 = st.columns([2, 6, 2])
     
-    # Display player information
-    st.markdown(f"**Team:** {filtered_data['Current Club'].iloc[0]}")
-    st.markdown(f"**Age:** {filtered_data['Age'].iloc[0]}")
-    st.markdown(f"**Position:** {filtered_data['Position'].iloc[0]}")
-    st.markdown(f"**Contract:** {filtered_data['Contract'].iloc[0]}")
-    st.markdown(f"**Agent:** {filtered_data['Agent'].iloc[0]}")
-    st.markdown(f"**Height:** {filtered_data['Average Height'].iloc[0]}")
-    st.markdown(f"**Foot:** {filtered_data['Foot'].iloc[0]}")
-    st.markdown(f"**Transfermarkt:** {filtered_data['Transfermarkt URL'].iloc[0]}")
+    with col1:
+        # Extract image URL from the cell
+        image_url = filtered_data['Image'].iloc[0]
+        if not pd.isnull(image_url):  # Check if image_url is not NaN
+            # Fetch image from Google Drive
+            response = requests.get(image_url)
+            if response.status_code == 200:
+                st.image(response.content, width=140)
+            else:
+                st.write("Image not available")
+        else:
+            st.write("No image available")
+    
+    # Display player information in the center column
+    with col2:
+        st.markdown(f"**Team:** {filtered_data['Current Club'].iloc[0]}")
+        st.markdown(f"**Age:** {filtered_data['Age'].iloc[0]}")
+        st.markdown(f"**Position:** {filtered_data['Position'].iloc[0]}")
+        st.markdown(f"**Contract:** {filtered_data['Contract'].iloc[0]}")
+        st.markdown(f"**Agent:** {filtered_data['Agent'].iloc[0]}")
+        st.markdown(f"**Height:** {filtered_data['Average Height'].iloc[0]}")
+        st.markdown(f"**Foot:** {filtered_data['Foot'].iloc[0]}")
+        st.markdown(f"**Transfermarkt:** {filtered_data['Transfermarkt URL'].iloc[0]}")
+
+    # Display additional player information in the third column
+    with col3:
+        st.markdown(f"**No. of Reports:** {filtered_data['No. of Reports'].iloc[0]}")
+        st.markdown(f"**A Verdicts:** {filtered_data['A Verdicts'].iloc[0]}")
+        st.markdown(f"**B Verdicts:** {filtered_data['B Verdicts'].iloc[0]}")
+        st.markdown(f"**ET Verdicts:** {filtered_data['ET Verdicts'].iloc[0]}")
+        st.markdown(f"**Sign Verdicts:** {filtered_data['Sign Verdicts'].iloc[0]}")
+        st.markdown(f"**Monitor Closely Verdicts:** {filtered_data['Monitor Closely Verdicts'].iloc[0]}")
+        st.markdown(f"**Average Player Performance:** {filtered_data['Average Player Performance'].iloc[0]}")
     
     st.markdown("---")  # Add a separator
 
@@ -1108,7 +1115,7 @@ def streamlit_interface():
     # Convert 'Match Performance' column to numeric
     report_data['Match Performance'] = pd.to_numeric(report_data['Match Performance'])
     
-    # Plot line plot based on 'Match Performance' for the last 5 reports
+    # Plot scatter plot based on 'Match Performance' for the last 5 reports
     fig = px.scatter(report_data, x='Date of report', y='Match Performance', 
                      title='Match Performance Over Last 5 Reports',
                      labels={'Date of report': 'Date', 'Match Performance': 'Match Performance'})
@@ -1118,23 +1125,10 @@ def streamlit_interface():
     fig.update_layout(width=800, height=600)  # Set plot size
     
     st.plotly_chart(fig)  # Display the plot
-    
-    st.markdown("---")  # Add a separator
-    
-    # Display additional player information
-    st.markdown(f"**No. of Reports:** {filtered_data['No. of Reports'].iloc[0]}")
-    st.markdown(f"**A Verdicts:** {filtered_data['A Verdicts'].iloc[0]}")
-    st.markdown(f"**B Verdicts:** {filtered_data['B Verdicts'].iloc[0]}")
-    st.markdown(f"**ET Verdicts:** {filtered_data['ET Verdicts'].iloc[0]}")
-    st.markdown(f"**Sign Verdicts:** {filtered_data['Sign Verdicts'].iloc[0]}")
-    st.markdown(f"**Monitor Closely Verdicts:** {filtered_data['Monitor Closely Verdicts'].iloc[0]}")
-    st.markdown(f"**Average Player Performance:** {filtered_data['Average Player Performance'].iloc[0]}")
 
-    st.markdown("---")  # Add a separator
-    
     # Center align the text "**Player Reports:**"
     st.markdown(f"### Player Reports ###", unsafe_allow_html=True)
-    
+        
     for index, row in filtered_data1[['Player', 'Scout', 'Comments', 'Date of report', 'Player Level - Score']].iterrows():
         st.markdown(f"**Player:** {row['Player']}")
         st.markdown(f"**Scout:** {row['Scout']}")
@@ -1142,7 +1136,7 @@ def streamlit_interface():
         st.markdown(f"**Verdict:** {row['Player Level - Score']}")
         st.markdown(f"**Comments:** {row['Comments']}")
         st.markdown("---")  # Add a separator
-      
+  
 # Load the DataFrame
 df = pd.read_csv("belgiumdata.csv")
 df2 = pd.read_csv("championshipscores.csv")
