72e1b5b9bf7c000e9fb9cd39b1952fe365af7df5
conorfroud <47294193+conorfroud@users.noreply.github.com>
2024-04-29 11:23:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1099,8 +1099,8 @@ def streamlit_interface():
         st.markdown(f"**No. of Reports:** {filtered_data['No. of Reports'].iloc[0]}")
         st.markdown(f"**A Verdicts:** {filtered_data['A Verdicts'].iloc[0]}")
         st.markdown(f"**B Verdicts:** {filtered_data['B Verdicts'].iloc[0]}")
-        st.markdown(f"**ET Verdicts:** {filtered_data['"ET" Verdicts'].iloc[0]}")
-        st.markdown(f"**"Sign" Verdicts:** {filtered_data['"Sign" Verdicts'].iloc[0]}")
+        st.markdown(f"**ET Verdicts:** {filtered_data['ET Verdicts'].iloc[0]}")
+        st.markdown(f"**"Sign" Verdicts:** {filtered_data['Sign Verdicts'].iloc[0]}")
         st.markdown(f"**Average Player Performance:** {filtered_data['Average Player Performance'].iloc[0]}")
     
     st.markdown("---")  # Add a separator
