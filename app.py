feea72e19f3e99fb9afe929b3221ab6e1bd26d71
conorfroud <47294193+conorfroud@users.noreply.github.com>
2024-06-21 14:59:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1250,7 +1250,7 @@ def display_data():
     st.markdown("<h3 style='text-align: center;'>Confidence Score Last Month</h3>", unsafe_allow_html=True)
 
     # Create three columns with adjusted widths
-    col1, col2, col3 = st.columns([0.8, 3, 1])
+    col1, col2, col3 = st.columns([1, 3, 1])
 
     # Display the table in the middle column
     with col2:
@@ -1297,7 +1297,7 @@ def plot_players_on_pitch(rb_players_data, lb_players_data, lw_players_data, rw_
     ax.get_yaxis().set_visible(False)
 
     # Create three columns with adjusted widths
-    col1, col2, col3 = st.columns([1, 3, 1])
+    col1, col2, col3 = st.columns([1, 5, 1])
 
     # Display the pitch plot in the middle column
     with col2:
