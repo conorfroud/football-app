9d3f731dabc08299225b510e77e62da44f61ec39
conorfroud <47294193+conorfroud@users.noreply.github.com>
2024-07-29 12:54:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2239,13 +2239,13 @@ def plot_players_on_pitch(rb_players_data, lb_players_data, lw_players_data, rw_
         for index, player in players_data.iterrows():
             ax.annotate(player['Player'], xy=(positions_x[position], start_y[position]), xytext=(positions_x[position], start_y[position]),
                         textcoords="offset points", ha='center', va='center', color='black', fontsize=5)
-            start_y[position] -= 2 
+            start_y[position] -= 3
 
     # Annotate left-footed CBs
     offset_left_cb = 0
     ax.annotate('CB', xy=(23, 32), xytext=(23, 32), textcoords="offset points", ha='center', va='center', color='black', fontsize=8, fontweight='bold')
     for index, player in left_cb_players_data.iterrows():
-        ax.annotate(player['Player'], xy=(23, 30), xytext=(25, 30 + offset_left_cb),
+        ax.annotate(player['Player'], xy=(23, 30), xytext=(23, 30 + offset_left_cb),
                     textcoords="offset points", ha='center', va='center', color='black', fontsize=5)
         offset_left_cb -= 15  # Adjust the offset for left-footed CBs
 
