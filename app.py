ba592e4356d79c43162325cd3794dac5bd4813c9
conorfroud <47294193+conorfroud@users.noreply.github.com>
2024-07-29 12:22:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2232,7 +2232,7 @@ def plot_players_on_pitch(rb_players_data, lb_players_data, lw_players_data, rw_
     # Annotate positions
     for position in ['RB', 'LB', 'LW', 'RW', 'CDM', 'CM', 'AM', 'CF']:
         ax.annotate(position, xy=(positions_x[position], position_label_y[position]), xytext=(positions_x[position], position_label_y[position]),
-                    textcoords="offset points", ha='center', va='center', color='red', fontsize=8, fontweight='bold')
+                    textcoords="offset points", ha='center', va='center', color='black', fontsize=8, fontweight='bold')
 
     # Annotate players for each position
     for position, players_data in zip(['RB', 'LB', 'LW', 'RW', 'CDM', 'CM', 'AM', 'CF'], [rb_players_data, lb_players_data, lw_players_data, rw_players_data, dm_players_data, cm_players_data, am_players_data, st_players_data]):
@@ -2243,15 +2243,15 @@ def plot_players_on_pitch(rb_players_data, lb_players_data, lw_players_data, rw_
 
     # Annotate left-footed CBs
     offset_left_cb = 0
-    ax.annotate('CB', xy=(2, 45), xytext=(21, 45), textcoords="offset points", ha='center', va='center', color='red', fontsize=8, fontweight='bold')
+    ax.annotate('CB', xy=(2, 45), xytext=(21, 45), textcoords="offset points", ha='center', va='center', color='black', fontsize=8, fontweight='bold')
     for index, player in left_cb_players_data.iterrows():
-        ax.annotate(player['Player'], xy=(2, 30), xytext=(21, 30 + offset_left_cb),
+        ax.annotate(player['Player'], xy=(2, 30), xytext=(23, 30 + offset_left_cb),
                     textcoords="offset points", ha='center', va='center', color='black', fontsize=5)
         offset_left_cb -= 15  # Adjust the offset for left-footed CBs
 
     # Annotate right-footed CBs
     offset_right_cb = 0
-    ax.annotate('CB', xy=(45, 45), xytext=(44, 45), textcoords="offset points", ha='center', va='center', color='red', fontsize=8, fontweight='bold')
+    ax.annotate('CB', xy=(45, 45), xytext=(44, 45), textcoords="offset points", ha='center', va='center', color='black', fontsize=8, fontweight='bold')
     for index, player in right_cb_players_data.iterrows():
         ax.annotate(player['Player'], xy=(45, 30), xytext=(44, 30 + offset_right_cb),
                     textcoords="offset points", ha='center', va='center', color='black', fontsize=5)
